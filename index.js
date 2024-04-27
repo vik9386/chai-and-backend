056a13010d62ef0e379eb719d33dfbf4b646e05a
vik9386 <kmr.vikash1994@gmail.com>
2024-04-27 06:46:32 +0530

    github added

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const githubData = {
+  "name":"vikash",
+  "location":"IN",
+  "pin":800020,
+} 
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -13,6 +19,10 @@ app.get("/twitter",(req,res)=>{
 
 app.get("/login",(req,res)=>{
     res.send("<h1>please login chai aur code</h1>")
+})
+
+app.get("/github",(req,res)=>{
+  res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
